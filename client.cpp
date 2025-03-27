ed3c01b680f8fc23a29a3833caa3da3340ebb1bc
Akshay Chouke <akshaychauke50@gmail.com>
2025-03-27 18:02:22 +0530

    added the crop and flip elements

--- client.cpp
+++ client.cpp
@@ -1,53 +1,66 @@
 #include <gst/gst.h>
 
+typedef struct _CustomData {
+  GstElement *pipeline;
+  GstElement *source;
+  GstElement *decoder;
+  GstElement *crop;
+  GstElement *flip;
+  GstElement *converter;
+  GstElement *sink;
+} CustomData;
+
 int main(int argc, char *argv[]) {
-    GstElement *pipeline, *source, *decoder, *converter, *sink;
+    CustomData data;
+
     GstBus *bus;
     GstMessage *msg;
     GstStateChangeReturn ret;
 
-    // Initialize GStreamer
     gst_init(&argc, &argv);
 
-    // Create elements
-    pipeline = gst_pipeline_new("camera-pipeline");
-    source = gst_element_factory_make("v4l2src", "source");
-    decoder = gst_element_factory_make("jpegdec", "decoder");
-    converter = gst_element_factory_make("videoconvert", "converter");
-    sink = gst_element_factory_make("autovideosink", "sink");
+    data.pipeline = gst_pipeline_new("camera-pipeline");
+    data.source = gst_element_factory_make("v4l2src", "source");
+    data.decoder = gst_element_factory_make("jpegdec", "decoder");
+    data.crop = gst_element_factory_make("videocrop", "crop");
+    data.flip = gst_element_factory_make("videoflip", "flip");
+    data.converter = gst_element_factory_make("videoconvert", "converter");
+    data.sink = gst_element_factory_make("autovideosink", "sink");
 
-    if (!pipeline || !source || !decoder || !converter || !sink) {
+    if (!data.pipeline || !data.source || !data.decoder || !data.crop || !data.flip || !data.converter || !data.sink) {
         g_printerr("Failed to create elements.\n");
         return -1;
     }
 
-    // Set properties
-    g_object_set(source, "device", "/dev/video0", NULL);
+    g_object_set(data.source, "device", "/dev/video0", NULL);
 
-    // Add elements to the pipeline
-    gst_bin_add_many(GST_BIN(pipeline), source, decoder, converter, sink, NULL);
+    g_object_set(data.crop,
+                 "top", 10,
+                 "bottom", 20,
+                 "left", 30,
+                 "right", 20,
+                 NULL);
 
+    g_object_set(data.flip, "method", 0, NULL);
 
-    // Link static elements
-    if (!gst_element_link_many(source, decoder, converter, sink, NULL)) {
+    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.decoder, data.crop, data.flip, data.converter, data.sink, NULL);
+
+    if (!gst_element_link_many(data.source, data.decoder, data.crop, data.flip, data.converter, data.sink, NULL)) {
         g_printerr("Failed to link elements.\n");
-        gst_object_unref(pipeline);
+        gst_object_unref(data.pipeline);
         return -1;
     }
 
-    // Start playback
-    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE) {
         g_printerr("Unable to set the pipeline to the playing state.\n");
-        gst_object_unref(pipeline);
+        gst_object_unref(data.pipeline);
         return -1;
     }
 
-    // Wait for error or EOS
-    bus = gst_element_get_bus(pipeline);
+    bus = gst_element_get_bus(data.pipeline);
     msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
-    // Process messages
     if (msg != NULL) {
         GError *err;
         gchar *debug_info;
@@ -69,10 +82,9 @@ int main(int argc, char *argv[]) {
         gst_message_unref(msg);
     }
 
-    // Cleanup
     gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
+    gst_element_set_state(data.pipeline, GST_STATE_NULL);
+    gst_object_unref(data.pipeline);
 
     return 0;
 }
